1f77e3e3ca8fa097ccf8634c4ca7aaea02ba532a
edomeru <alarteedmer@gmail.com>
2026-02-24 10:42:47 +0900

    Fix iOS google maps error

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,14 +2,13 @@ import Flutter
 import UIKit
 import GoogleMaps
 
-GMSServices.provideAPIKey("AIzaSyA-fyd6Kwg5R2H4ea-PFYI5ulsV7XTTmtQ")
-
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyA-fyd6Kwg5R2H4ea-PFYI5ulsV7XTTmtQ") // ✅ moved inside here
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
